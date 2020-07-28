6a629a525bd82159955d723cc5f38863b9ef79f2
Aaryaman Sen <54085618+aaryamansen@users.noreply.github.com>
2020-07-28 17:24:33 +0530

    updated bizdev graphics

--- src/components/header.js
+++ src/components/header.js
@@ -32,7 +32,7 @@ class Header extends React.Component {
         <div className="HeaderGroup">
             <Link to="/"><img src={require('../images/leftfield-logo.png')} width="160" /></Link>
             <div className="NavLinks">
-                <Link to="/services">Services</Link>, <Link to="/team">Team</Link>
+                <Link to="/services">Services</Link>, <Link to="/team">Team</Link>, <a href="#Contact">Contact</a>
             </div>
         </div>
       </div>
